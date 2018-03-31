9780844b69edd4fda4653210e60160f9dd3d5af9
Dean Hamann <hamann.dean@gmail.com>
2018-03-30 21:04:52 -0700

    Adds adjacentElementsProduct

--- March30.js
+++ March30.js
@@ -1,6 +1,6 @@
 // Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
 
-let sequence = [1,3,2,1];
+let sequence = [1, 3, 2, 1];
 
 function almostIncreasingSequence(sequence) {
   var found = false;
@@ -21,6 +21,21 @@ function almostIncreasingSequence(sequence) {
     }
   }
   return true;
+}
+
+// Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+let inputArray = [3, 6, -2, -5, 7, 3];
+
+function adjacentElementsProduct(inputArray) {
+  let highestSum = inputArray[0] * inputArray[1];
+  for (let i = 0; i < inputArray.length; i++) {
+    let contestedNumSum = inputArray[i] * inputArray[i + 1];
+    if (contestedNumSum > highestSum) {
+      highestSum = contestedNumSum;
+    }
+  }
+  return highestSum;
 }
 
 
