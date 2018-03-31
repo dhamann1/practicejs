1c94cc5eedc8e8c4d3cfd3e0ad9d388838abddbc
Dean Hamann <hamann.dean@gmail.com>
2018-03-30 20:45:34 -0700

    Adds almostIncreasingSequence

---
+++ March30.js
@@ -0,0 +1,26 @@
+// Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+
+let sequence = [1,3,2,1];
+
+function almostIncreasingSequence(sequence) {
+  var found = false;
+  for (var i = 0; i < sequence.length; i++) {
+    if (sequence[i] <= sequence[i - 1]) {
+      if (found) {
+        return false;
+      } else {
+        found = true;
+      }
+      if (i === 1 || i + 1 === sequence.length) {
+        continue;
+      } else if (sequence[i] > sequence[i - 2]) {
+        sequence[i - 1] = sequence[i - 2];
+      } else if (sequence[i - 1] >= sequence[i + 1]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
