4e55f839a6027adff30ed1e995baf659160ebf31
Dean Hamann <hamann.dean@gmail.com>
2018-04-01 18:36:04 -0700

    Adds firstDuplicate

--- April1.js
+++ April1.js
@@ -24,4 +24,15 @@ function persistence(num) {
   return count;
 }
 
+// Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
 
+function firstDuplicate(a) {
+  let numObj = {};
+  for (let i = 0; i < a.length; i++) {
+    if (numObj[a[i]] !== undefined) {
+      return a[i];
+    }
+    numObj[a[i]]++;
+  }
+  return -1;
+}
