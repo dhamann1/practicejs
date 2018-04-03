df7cefc40c7e999cc1a442405232d009acd246dd
Dean Hamann <hamann.dean@gmail.com>
2018-04-03 13:55:09 -0700

    Adds isValidWalk

--- April3.js
+++ April3.js
@@ -49,4 +49,15 @@ function duplicateEncode(word) {
   return newWord.join('');
 }
 
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+function isValidWalk(walk) {
+  let north = walk.filter(elem => elem === 'n').length;
+  let south = walk.filter(elem => elem === 's').length;
+  let east = walk.filter(elem => elem === 'e').length;
+  let west = walk.filter(elem => elem === 'w').length;
+  return walk.length === 10 && north === south && east === west;
+}
+
+
 
