e8f06f33405b747394572c8dedbb44c5e5f9b5c3
Dean Hamann <hamann.dean@gmail.com>
2018-04-03 12:44:19 -0700

    Adds duplicateEncode

--- April3.js
+++ April3.js
@@ -35,4 +35,18 @@ function iqTest(numbers) {
   return numbers.indexOf(evenNumbers[0]);
 }
 
+// The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+function duplicateEncode(word) {
+  word = word.toLowerCase().split('');
+  let newWord = word.map(elem => {
+    if (word.indexOf(elem) == word.lastIndexOf(elem)) {
+      return elem = "(";
+    } else {
+      return elem = ")";
+    }
+  });
+  return newWord.join('');
+}
+
 
