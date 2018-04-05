3cd0e77888d938dd43966ba7170f53dae3d0177a
Dean Hamann <hamann.dean@gmail.com>
2018-04-05 16:34:55 -0700

    Adds songDecoder

--- April5.js
+++ April5.js
@@ -15,3 +15,10 @@ function findOdd(a) {
     }
   }
 }
+
+
+// Let's assume that a song consists of some number of words. To make the dubstep remix of this song, Polycarpus inserts a certain number of words "WUB" before the first word of the song (the number may be zero), after the last word (the number may be zero), and between words (at least one between any pair of neighbouring words), and then the boy glues together all the words, including "WUB", in one string and plays the song at the club. Help restore the original song title.
+
+function songDecoder(str) {
+  return str.replace(/(WUB)+/g, " ").trim();
+}
