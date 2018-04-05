853a61d50ffd6cc34742f9ceb605cf67582d9dde
Dean Hamann <hamann.dean@gmail.com>
2018-04-04 22:11:06 -0700

    Adds findChildren

--- April4.js
+++ April4.js
@@ -4,3 +4,17 @@ function factorialRecursion(n) {
   if (n === 0) return 1;
   return n * factorial(n - 1);
 }
+
+// Place all people in alphabetical order where Mothers are followed by their children.I.E "aAbaBb" => "AaaBbb".
+
+function findChildren(dancingBrigade) {
+  char = "";
+  let arr = dancingBrigade.toLowerCase().split("").sort().map(item => {
+    if (item != char) {
+      char = item;
+      return item.toUpperCase();
+    }
+    return item;
+  }).join("");
+  return arr;
+};
