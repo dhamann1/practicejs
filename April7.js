eb0a6118f5231c1a0859c9f6ad8aa8ac8d80a6a9
Dean Hamann <hamann.dean@gmail.com>
2018-04-07 13:47:14 -0700

    Adds spinWords

---
+++ April7.js
@@ -0,0 +1,8 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+function spinWords(words) {
+  return words.split(' ').map(word => {
+    return (word.length > 4) ? word.split('').reverse().join('') : word;
+  }).join(' ');
+}
+
