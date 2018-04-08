f1e29399f0764addf92b94c22d0a2e7765ca9f4f
Dean Hamann <hamann.dean@gmail.com>
2018-04-07 17:06:51 -0700

    Adds sumPairs

--- April7.js
+++ April7.js
@@ -35,3 +35,16 @@ function uniqueInOrder(iterable) {
 }
 
 
+
+// Given a list of integers and a single sum value, return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+
+function sumPairs(ints, s) {
+  let seen = {};
+  for (let i = 0; i < ints.length; i++) {
+    if (seen[s - ints[i]]) {
+      return [s - ints[i], ints[i]];
+    }
+    seen[ints[i]] = true;
+  }
+  return undefined;
+}
