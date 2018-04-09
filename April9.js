92cda573b2f5df16681d01e247cfc7d269a43092
Dean Hamann <hamann.dean@gmail.com>
2018-04-09 16:40:19 -0700

    Adds generateHashtag

--- April9.js
+++ April9.js
@@ -16,3 +16,24 @@ function tribonacci(signature, n) {
   }
   return signature.slice(0, n);
 }
+
+// The marketing team are spending way too much time typing in hashtags.
+// Let's help them with out own Hashtag Generator!
+
+// Here's the deal:
+
+// If the final result is longer than 140 chars it must return false.
+// If the input is a empty string it must return false.
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+// Example Input to Output:
+
+// " Hello there thanks for trying my Kata" => "#HelloThereThanksForTryingMyKata"
+// " Hello World " => "#HelloWorld"
+
+function generateHashtag(str) {
+  return str.length > 140 || str === '' ? false :
+    '#' + str.split(' ').map(word => {
+      return word[0].toUpperCase() + word.slice(1);
+    }).join('');
+}
