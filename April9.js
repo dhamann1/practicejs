9c8d4fc8909fe604f8435502da444629ef130d5b
Dean Hamann <hamann.dean@gmail.com>
2018-04-09 22:17:26 -0700

    Adds findNB

--- April9.js
+++ April9.js
@@ -37,3 +37,19 @@ function generateHashtag(str) {
       return word[0].toUpperCase() + word.slice(1);
     }).join('');
 }
+
+
+// Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+
+// You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+
+function findNB(m) {
+  let n = 0;
+  while (m > 0) {
+    n++;
+    m -= n ** 3;
+  }
+  return m ? -1 : n;
+}
+
+
