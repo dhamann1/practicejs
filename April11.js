2b104de9dcafa2e41d9715a446f5227807802d78
Dean Hamann <hamann.dean@gmail.com>
2018-04-11 21:02:59 -0700

    Adds equallyStrong

--- April11.js
+++ April11.js
@@ -54,3 +54,14 @@ function palindromeRearranging(inputString) {
   }
   return true;
 }
+
+// Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+// Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+
+function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
+  if (yourLeft === friendsLeft && yourRight === friendsRight || yourLeft ===
+    friendsRight && friendsLeft === yourRight) {
+    return true;
+  }
+  return false;
+}
