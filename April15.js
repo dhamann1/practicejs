cd2dcc3f5a83fba141b948411367a1ab82d9d8ec
Dean Hamann <hamann.dean@gmail.com>
2018-04-17 00:16:25 -0700

    Adds twoSum

--- April15.js
+++ April15.js
@@ -46,3 +46,13 @@ function toWeirdCase(string) {
     }).join('');
   }).join(' ');
 }
+
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in an array like so: [index1, index2].
+
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) return [i, j];
+    }
+  }
+}
