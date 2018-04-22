37afc0f80486d388df9504f039e4efa51af0a788
Dean <hamann.dean@gmail.com>
2018-04-22 16:47:22 -0700

    Adds splitTheBill

--- OOP.js
+++ OOP.js
@@ -68,3 +68,28 @@ Object.prototype.hash = function(string) {
   }
   return current;
 }
+
+// It's tricky keeping track of who is owed what when spending money in a group. Write a function to balance the books.
+//
+// The function should take one parameter: an object/dict with two or more name-value pairs which represent the members of the group and the amount spent by each.
+// The function should return an object/dict with the same names, showing how much money the members should pay or receive.
+// Further points:
+//
+// The values should be positive numbers if the person should receive money from the group, negative numbers if they owe money to the group.
+// If value is a decimal, round to two decimal places.
+
+function splitTheBill(x) {
+  let sum = 0;
+  let groupNum = 0;
+  let fairAmount = 0;
+  let newObj = {};
+  for (key in x) {
+    groupNum++;
+    sum += x[key];
+  }
+  fairAmount = sum / groupNum;
+  for (item in x) {
+    newObj[item] = Math.round((x[item] - fairAmount) * 100) / 100;
+  }
+  return newObj;
+}
