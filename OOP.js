00648a332fec09d91621639b25bb03b8958009fd
Dean <hamann.dean@gmail.com>
2018-04-22 15:27:21 -0700

    Adds SubstitutionCipher

--- OOP.js
+++ OOP.js
@@ -1,7 +1,7 @@
-function createNewPerson (name){
+function createNewPerson(name) {
   var obj = {};
   obj.name = name;
-  obj.greeting = function (){
+  obj.greeting = function() {
     console.log('Hi! I\'m ' + this.name + '.');
   }
   return obj;
@@ -13,18 +13,18 @@ salva.greeting();
 
 //shortcut using constructor function
 
-function Person(name){
+function Person(name) {
   this.name = name;
-  this.greeting = function(){
+  this.greeting = function() {
     console.log('Hi! I\'m ' + this.name + '.');
   }
 }
 
 var person1 = new Person("Dean");
-var person2 = new Person ("Mark");
+var person2 = new Person("Mark");
 
 
-function Person (first, last, age, gender, interests) {
+function Person(first, last, age, gender, interests) {
   this.name = {
     first,
     last
@@ -35,7 +35,27 @@ function Person (first, last, age, gender, interests) {
 };
 
 
-function Teacher (first, last, age, gender, interests, subject){
+function Teacher(first, last, age, gender, interests, subject) {
   Person.call(this, first, last, age, gender, interests);
   this.subject = subject;
+}
+
+/* A simple substitution cipher replaces one character from an alphabet with
+a character from an alternate alphabet, where each character's position in an
+ alphabet is mapped to the alternate alphabet for encoding or decoding. */
+
+let abc1 = "abcdefghijklmnopqrstuvwxyz";
+let abc2 = "etaoinshrdlucmfwypvbgkjqxz";
+
+function SubstitutionCipher(abc1, abc2) {
+  this.encode = function(str) {
+    return encodedStr = str.split('').map(function(elem) {
+      return elem = abc2[abc1.indexOf(elem)];
+    }).join('');
+  }
+  this.decode = function(str) {
+    return encodedStr = str.split('').map(function(elem) {
+      return elem = abc1[abc2.indexOf(elem)];
+    }).join('');
+  }
 }
