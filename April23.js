2ee4de2e46117ec3b41b7068de5145514f3f2e36
Dean Hamann <hamann.dean@gmail.com>
2018-04-24 22:23:52 -0700

    Adds narcissistic

--- April23.js
+++ April23.js
@@ -4,3 +4,11 @@ function inArray(a1, a2) {
   let str = a2.join(' ');
   return a1.filter(s => str.indexOf(s) !== -1).sort();
 }
+
+// Your code must return true or false depending upon whether the given number is a Narcissistic number.
+
+function narcissistic(value) {
+  let newValue = value.toString().split('');
+  let returnValue = newValue.map(num => Math.pow(Number(num), newValue.length)).reduce((a, b) => a + b);
+  return returnValue === value;
+}
