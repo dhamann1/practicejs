3649c79d42a1a26e17fc098a5ebeeb09fbd9a129
Dean Hamann <hamann.dean@gmail.com>
2018-04-26 23:35:30 -0700

    Adds multiples3or5

--- April23.js
+++ April23.js
@@ -25,3 +25,24 @@ function towerBuilder(nFloors) {
   }
   return tower;
 }
+
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function multiples3or5(number) {
+  let sum = 0;
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      sum += i;
+    } else if (i % 3 === 0) {
+      sum += i;
+    } else if (i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
