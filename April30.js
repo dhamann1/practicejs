96b5e88bd38702df676afc82ac1823d9e08bcf48
Dean <hamann.dean@gmail.com>
2018-05-01 18:55:38 -0700

    Adds titleCase

--- April30.js
+++ April30.js
@@ -67,3 +67,18 @@ function createFunctions(n) {
   }
   return callbacks;
 }
+
+// A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+
+// Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+function titleCase(title, minorWords) {
+  minorWords = minorWords ? minorWords.split(' ').map(w => w.toLowerCase()) : minorWords;
+  return title.split(' ').map((word, i) => {
+    if (minorWords && minorWords.includes(word.toLowerCase()) && i !== 0) {
+      return word.toLowerCase();
+    } else if (word.split('').length) {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }
+  }).join(' ');
+}
