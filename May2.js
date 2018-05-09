63aff54124ad04b1cc0b6fedae831c6c40ccbf0f
Dean <hamann.dean@gmail.com>
2018-05-08 23:27:28 -0700

    Adds shiftedDiff

--- May2.js
+++ May2.js
@@ -108,3 +108,11 @@ function getWinner(list) {
   });
   return answer[0] || null;
 }
+
+// Write a function that receives two strings and returns n, where n is equal to the number of characters we should shift the first string forward to match the second.
+// For instance, take the strings "fatigue" and "tiguefa". In this case, the first string has been rotated 5 characters forward to produce the second string, so 5 would be returned.
+// If the second string isn't a valid rotation of the first string, the method returns -1.
+
+function shiftedDiff(first, second) {
+  return (first.length == second.length) ? (second + second).indexOf(first) : -1;
+}
