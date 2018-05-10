2e3f724ecd96c6620997f4ee4380331e61a21322
Dean Hamann <hamann.dean@gmail.com>
2018-05-09 21:16:57 -0700

    Adds endsWith

--- May2.js
+++ May2.js
@@ -118,3 +118,13 @@ function noOdds( values ){
     return number % 2 === 0
   });
 }
+
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+//Easy way 
+
+function endsWith(str, ending){
+  return str.endsWith(ending);
+}
+
