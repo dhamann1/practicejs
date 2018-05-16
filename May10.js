5cbfba479628c551c4655fa86fb1156bb6196fa6
Dean Hamann <hamann.dean@gmail.com>
2018-05-15 21:37:02 -0700

    Adds scoreThrows

--- May10.js
+++ May10.js
@@ -63,3 +63,37 @@ function firstNSmallest(array, n) {
   }
   return result;
 }
+
+// You've just recently been hired to calculate scores for a Dart Board game!
+// Scoring specifications:
+// 0 points - radius above 10
+// 5 points - radius between 5 and 10 inclusive
+// 10 points - radius less than 5
+// If all radiuses are less than 5, award 100 BONUS POINTS!
+// Write a function that accepts an array of radiuses (can be integers and/or floats), and returns a total score using the above specification.
+// An empty array should return 0.
+
+
+function scoreThrows(radiuses) {
+  if (radiuses.length <= 0) {
+    return 0;
+  }
+  let score = 0
+  let count = true
+  for (let i = 0; i < radiuses.length; i++) {
+    if (radiuses[i] < 5) {
+      score += 10;
+    } else if (radiuses[i] <= 10) {
+      score += 5;
+      count = false;
+    } else {
+      count = false;
+    }
+  }
+
+  if (count) {
+    score += 100
+  }
+
+  return score;
+}
