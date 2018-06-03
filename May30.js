732cebeb8aa1eb21a5dbb62437c360b500d991ec
Dean <hamann.dean@gmail.com>
2018-06-02 23:09:45 -0700

    Adds fatFingers

--- May30.js
+++ May30.js
@@ -12,6 +12,7 @@ function minimumNumber(numbers) {
   for (let i = sum;; i++) {
     if (prime(i)) return i - sum
   }
+
   function prime(a) {
     if (a < 2) return false
     if (a % 2 === 0) return a === 2
@@ -21,3 +22,24 @@ function minimumNumber(numbers) {
     return true
   }
 }
+
+Freddy has a really fat left pinky finger, and every time Freddy tries to type an A, he accidentally hits the CapsLock key!
+
+// Given a string that Freddy wants to type, emulate the keyboard misses where each A supposedly pressed is replaced with CapsLock, and return the string that Freddy actually types. It doesn't matter if the A in the string is capitalized or not. When CapsLock is enabled, capitalization is reversed, but punctuation is not affected.
+
+var fatFingers = function(str) {
+  if (!str) return str;
+  let isCapsLocked = false;
+  let result = str.split('').reduce(function(acc, char) {
+    if (char.toUpperCase() === 'A') {
+      isCapsLocked = !isCapsLocked;
+      return acc;
+    }
+    if (isCapsLocked) {
+      char = char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase();
+    }
+    acc += char;
+    return acc;
+  }, '');
+  return result;
+};
