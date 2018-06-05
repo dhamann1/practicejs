995e8012ea9697552e83ea06a6d7ef24aaae4d02
Dean <hamann.dean@gmail.com>
2018-06-04 22:45:15 -0700

    Adds alphabetized

--- May30.js
+++ May30.js
@@ -60,3 +60,19 @@ function bracketFinder(str, idx) {
       count -= 1
   }
 }
+
+// Re-order the characters of a string, so that they are concatenated into a new string in "case-insensitively-alphabetical-order-of-appearance" order. Whitespace and punctuation shall simply be removed!
+// The input is restricted to contain no numerals and only words containing the english alphabet letters.
+
+function alphabetized(s) {
+  let results = "";
+  s = s.replace(/\s/g, '');
+  'abcdefghijklmnopqrstuvwxyz'.split('').forEach((item) => {
+    s.split('').forEach((letter) => {
+      if (item == letter.toLowerCase()) {
+        results += letter;
+      }
+    });
+  });
+  return results;
+}
