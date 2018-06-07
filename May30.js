6cfc22eeab57fbcaa913d93522f0df914c4de496
Dean <hamann.dean@gmail.com>
2018-06-06 21:14:23 -0700

    Adds cogRpm

--- May30.js
+++ May30.js
@@ -87,3 +87,18 @@ function validate(n) {
     return sum + digit;
   }, 0) % 10 == 0;
 }
+
+
+
+
+
+// You are given a list of cogs in a gear train
+// Each element represents the number of teeth of that cog
+// e.g. [100, 75] means
+// 1st cog has 100 teeth
+// 2nd cog has 75 teeth
+// If the first cog rotates clockwise at 1 RPM what is the RPM of the final cog?
+
+function cogRpm(cogs) {
+  return (cogs.length % 2 == 0 ? -1 : 1) * cogs[0] / cogs[cogs.length-1];
+}
