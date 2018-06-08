b6da60cb4b4aa25bd90bd1c65a23f77068914ef2
Dean <hamann.dean@gmail.com>
2018-06-07 19:54:02 -0700

    Adds sortArray

--- June7.js
+++ June7.js
@@ -1,3 +1,23 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 // Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function multiples3or5(number) {
+  let count = 0;
+  let sum = 0;
+  while (count < number) {
+    if (count % 3 == 0 || count % 5 == 0) {
+      sum += count;
+    }
+    count++;
+  }
+  return sum;
+}
+
+// Your task is to sort ascending odd numbers but even numbers must be on their places.
+// Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+
+function sortArray(array) {
+  let odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+  return array.map((x) => x % 2 ? odd.shift() : x);
+}
