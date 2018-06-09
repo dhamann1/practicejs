a44e9477afc3f08c46527da8f1d3460135392455
Dean <hamann.dean@gmail.com>
2018-06-08 17:19:40 -0700

    Adds caffeineBuzz

--- June7.js
+++ June7.js
@@ -21,3 +21,16 @@ function sortArray(array) {
   let odd = array.filter((x) => x % 2).sort((a,b) => a - b);
   return array.map((x) => x % 2 ? odd.shift() : x);
 }
+
+// Complete the function caffeineBuzz, which takes a non-zero integer as it's one argument.
+// If the integer is divisible by 3, return the string "Java".
+// If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+// If the integer is one of the above and is even, add "Script" to the end of the string.
+// Otherwise, return the string "mocha_missing!"
+
+function caffeineBuzz(n){
+  if (n % 12 === 0) return "CoffeeScript";
+  if (n % 6 === 0) return "JavaScript";
+  if (n % 3 === 0)  return "Java";
+  return "mocha_missing!";
+}
