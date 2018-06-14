d16af6a65e47d5e3912f96b1fa17f5214f47fa1d
Dean <hamann.dean@gmail.com>
2018-06-13 20:16:10 -0700

    Adds sumDigits

--- June7.js
+++ June7.js
@@ -81,3 +81,11 @@ function keysAndValues(data) {
 function sortGiftCode(code){
   return code.split('').sort().join('');
 }
+
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+function sumDigits(number) {
+  let sum = 0;
+  Math.abs(number).toString().split('').map(num => sum += Math.abs(num))
+  return sum;
+}
