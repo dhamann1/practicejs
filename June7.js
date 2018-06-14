03efcfc5fa72fb8cce357719397086480b8fa9e4
Dean <hamann.dean@gmail.com>
2018-06-13 20:09:10 -0700

    Adds sortGiftCode

--- June7.js
+++ June7.js
@@ -75,3 +75,9 @@ function keysAndValues(data) {
   }
   return [keys, vals];
 }
+
+// Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+function sortGiftCode(code){
+  return code.split('').sort().join('');
+}
