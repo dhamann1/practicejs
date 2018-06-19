d8430382d48b58e4dfbc638acc0e4f5c48148429
Dean <hamann.dean@gmail.com>
2018-06-18 19:46:19 -0700

    Adds cookingTime

--- June17.js
+++ June17.js
@@ -27,3 +27,14 @@ function getEvenNumbers(numbersArray){
     return n % 2 === 0;
   });
 }
+
+// Implement a function, which takes a non-negative integer, representing the number of eggs to boil. It must return the time in minutes (integer), which it takes to have all the eggs boiled.
+// Rules
+// you can put at most 8 eggs into the pot at once
+// it takes 5 minutes to boil an egg
+// we assume, that the water is boiling all the time (no time to heat up)
+// for simplicity we also don't consider the time it takes to put eggs into the pot or get them out of it
+
+function cookingTime (eggs) {
+  return 5 * Math.ceil(eggs / 8);
+}
