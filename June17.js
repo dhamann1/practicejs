bc62ba5999d5b01a8a18ca35f4a059092c2a90a0
Dean <hamann.dean@gmail.com>
2018-06-25 20:57:40 -0700

    Adds Warrior

--- June17.js
+++ June17.js
@@ -123,3 +123,18 @@ function findEmployeesRole(name) {
 function pluck(objs, name) {
   return objs.map((obj) => obj[name]);
 }
+
+// Something is wrong with our Warrior class. The strike method does not work correctly. The following shows an example of this code being used:
+// var ninja = new Warrior('Ninja');
+// var samurai = new Warrior('Samurai');
+// samurai.strike(ninja, 3);
+// // ninja.health should == 70
+
+var Warrior = function(name){
+  this.name = name;
+  this.health = 100;
+}
+
+Warrior.prototype.strike = function(enemy, swings){
+  enemy.health = Math.max(0, enemy.health - (swings * 10));
+}
