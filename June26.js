539d913ad11df997f258fbfc758afe8f2c28e424
Dean <hamann.dean@gmail.com>
2018-07-03 16:06:52 -0700

    Adds isSorted

--- June26.js
+++ June26.js
@@ -55,7 +55,7 @@ function mostFrequentItemCount(collection) {
 
   var count = {};
 
-  collection.forEach( item => {
+  collection.forEach(item => {
     count[item] = (count[item] || 0) + 1;
   });
 
@@ -64,6 +64,35 @@ function mostFrequentItemCount(collection) {
 
 
 
-function multiplyChars(number){
-  return "hue".repeat(number);
+function multiplyChars(word, number) {
+  return word.repeat(number);
+}
+
+// Complete the method which accepts an array of integers, and returns one of the following:
+//
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+
+function isSortedAndHow(array) {
+  var ans;
+  if (array[1] > array[0]) {
+    ans = "yes, ascending";
+  }
+
+  if (ans === "yes, ascending") {
+    for (var i = 1; i < array.length; i++) {
+      if (array[i] >= array[i - 1]) {
+        ans = "yes, ascending";
+      } else return ans = "no";
+    }
+  }
+  if (ans !== "yes, ascending") {
+    for (var i = 1; i < array.length; i++) {
+      if (array[i] <= array[i - 1]) {
+        ans = "yes, descending";
+      } else return ans = "no";
+    }
+  }
+  return ans;
 }
