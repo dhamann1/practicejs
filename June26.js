6b09f9e0a72cadf6fd7be712e84db75cbf6794f4
Dean <hamann.dean@gmail.com>
2018-07-04 12:36:23 -0700

    Adds isSquare

--- June26.js
+++ June26.js
@@ -96,3 +96,11 @@ function isSortedAndHow(array) {
   }
   return ans;
 }
+
+
+
+// Given an integral number, determine if it's a square number:
+
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
