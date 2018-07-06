383b01a32cfdf750de4ffdf50a26383a13945c00
Dean <hamann.dean@gmail.com>
2018-07-05 21:06:36 -0700

    Adds twoOldestAges

--- June26.js
+++ June26.js
@@ -97,10 +97,14 @@ function isSortedAndHow(array) {
   return ans;
 }
 
-
-
 // Given an integral number, determine if it's a square number:
 
 function isSquare(n) {
   return Math.sqrt(n) % 1 === 0;
+}
+
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+function twoOldestAges(ages){
+  return ages.sort(function(a,b){return a-b;}).slice(-2);
 }
