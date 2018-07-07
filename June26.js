0e90d56e738532cfe583207a25b882f086b33dfc
Dean <hamann.dean@gmail.com>
2018-07-07 07:25:30 -0700

    Adds sum

--- June26.js
+++ June26.js
@@ -120,3 +120,9 @@ Array.prototype.reverse = function() {
 
   return this;
 };
+
+// Your job is to add a method 'sum' to all array objects, which sums all the numbers in it. You should return 0 if the array is empty.
+
+Array.prototype.sum = function() {
+  return this.reduce((a, b) => a + b, 0);
+};
