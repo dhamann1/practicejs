dd480e39510d2c140aaa1bcf3f43b46f64613fde
Dean <hamann.dean@gmail.com>
2018-07-15 18:58:28 -0700

    Adds last

--- July14.js
+++ July14.js
@@ -3,3 +3,14 @@
 function sum(...numbers) {
   return numbers.reduce((x, y) => x + y);
 }
+
+// Find the last element of the given argument(s).
+
+function last(list){
+  var out = list;
+  if(arguments.length > 1)
+    list = arguments;
+  if(list.length > 1)
+    out = list[list.length-1];
+  return out;
+}
