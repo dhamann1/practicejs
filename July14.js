2e1ff7168d446c34ee774b5a8e099a036f5504f0
Dean <hamann.dean@gmail.com>
2018-07-16 21:01:42 -0700

    Adds removeRotten

--- July14.js
+++ July14.js
@@ -6,11 +6,23 @@ function sum(...numbers) {
 
 // Find the last element of the given argument(s).
 
-function last(list){
+function last(list) {
   var out = list;
-  if(arguments.length > 1)
+  if (arguments.length > 1)
     list = arguments;
-  if(list.length > 1)
-    out = list[list.length-1];
+  if (list.length > 1)
+    out = list[list.length - 1];
   return out;
+}
+
+// Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+
+
+function removeRotten(bagOfFruits) {
+  if (!bagOfFruits || bagOfFruits === []) {
+    return [];
+  }
+  return bagOfFruits.map(fruit =>
+    fruit.replace(/rotten/, () => '').toLowerCase()
+  );
 }
