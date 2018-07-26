11fe65ad905ec94d26afd2f2ea82702df68954be
Dean Hamann <hamann.dean@gmail.com>
2018-07-25 22:36:33 -0700

    Adds sumArray

--- July14.js
+++ July14.js
@@ -98,3 +98,19 @@ function pizzaPayments(costs) {
 function positiveSum(arr) {
   return arr.filter(x => x > 0).reduce((a, b) => a+b, 0);
 }
+
+
+// Sum all the numbers of the array except the highest and the lowest element (the value, not the index!).
+
+function sumArray(array) {
+  if (array == null || array.length <= 2) {
+    return 0
+  }
+  let max = Math.max.apply(Math, array);
+  let min = Math.min.apply(Math, array);
+  let sum = 0
+  for (i = 0; i < array.length; i++) {
+    sum += array[i];
+   }
+  return sum - max - min
+}
