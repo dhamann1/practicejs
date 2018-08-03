33c2c1a2fcc6f353276959e8c886f11f22c134d4
Dean <hamann.dean@gmail.com>
2018-08-02 19:25:45 -0700

    Adds AmIAfraid

---
+++ August2.js
@@ -0,0 +1,31 @@
+// I've got a crazy mental illness. I dislike numbers a lot. But it's a little complicated: The number I'm feared of depends on which day of week it is... This a concrete description of my mental illness:
+//
+// Monday --> 12
+//
+// Tuesday --> numbers greater than 95
+//
+// Wednesday --> 34
+//
+// Thursday --> 0
+//
+// Friday --> numbers divisable by 2
+//
+// Saturday --> 56
+//
+// Sunday --> 666 or -666
+//
+// Write a function which takes a string (day of week) and an integer (number to be tested) so it tells the doctor if I'm feared or not. (return a boolean)
+
+
+
+let AmIAfraid = (day, num) => {
+  return {
+    Monday: d => d == 12,
+    Tuesday: d => d > 95,
+    Wednesday: d => d == 34,
+    Thursday: d => !d,
+    Friday: d => !(d % 2),
+    Saturday: d => d == 56,
+    Sunday: d => Math.abs(d) == 666
+  }[day](num);
+}
