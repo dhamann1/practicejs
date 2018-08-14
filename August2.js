33a9f4a1cd24920d4aff07b078d7e85f085cda62
Dean Hamann <hamann.dean@gmail.com>
2018-08-13 22:24:24 -0700

    Adds calculateAge

--- August2.js
+++ August2.js
@@ -102,3 +102,25 @@ function Sleigh() {}
 Sleigh.prototype.authenticate = function(name, password) {
   return name === "Santa Claus" && password === "Ho Ho Ho!";
 };
+
+
+// Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+// Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: "You were born this very year!"
+// "..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.
+
+function calculateAge(a, b) {
+  if (a - b == 0) {
+    return 'You were born this very year!';
+  }
+  if (b - a == 1) {
+    return `You are ${b-a} year old.`;
+  }
+  if (b > a) {
+    return `You are ${b-a} years old.`;
+  }
+  if (a - b == 1) {
+    return `You will be born in ${a-b} year.`;
+  } else {
+    return `You will be born in ${a-b} years.`;
+  }
+}
