fc17112879dbadf9e1dd8f59de6d634946d6f90b
Dean <hamann.dean@gmail.com>
2018-08-26 13:19:58 -0700

    Adds finalGrade

--- August2.js
+++ August2.js
@@ -186,3 +186,17 @@ const countSheep = num => {
   }
   return val;
 }
+
+// This function should return a number (final grade). There are four types of final grades:
+//
+// 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// 0, in other cases
+
+const finalGrade = (exam, projects) => {
+  if(exam > 90 || projects > 10) return 100
+  if(exam > 75 && projects > 4) return 90
+  if(exam > 50 && projects > 1) return 75
+  return 0;
+}
