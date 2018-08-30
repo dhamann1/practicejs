2e301b47d540ec99ca18a738058c923a8210f15d
Dean <hamann.dean@gmail.com>
2018-08-29 20:57:13 -0700

    Adds rentalCarCost

--- August2.js
+++ August2.js
@@ -233,3 +233,17 @@ function switchItUp(number) {
       return 'Unknown number';
   }
 }
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+//
+// Write a code that gives out the total amount for different days(d).
+
+function rentalCarCost(d) {
+  var total=40*d,discount=0;
+  if(d>=7){
+    total-=50;
+  }else if(d>=3){
+    total-=20;
+  }
+  return total;
+}
