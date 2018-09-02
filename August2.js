1defdef4973ba326bf60b33b5f72d0134d160818
Dean Hamann <hamann.dean@gmail.com>
2018-09-02 13:52:05 -0700

    Adds removeChar

--- August2.js
+++ August2.js
@@ -286,3 +286,9 @@ function calculateTip(amount, rating) {
   return rating in tips ? Math.ceil(amount * tips[rating]) : "Rating not recognised";
 
 }
+
+// create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str) {
+  return str.slice(1, -1);
+}
