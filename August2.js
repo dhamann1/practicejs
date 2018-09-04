d7763d6ead45b29de0fe3f3e54c479976362ac21
Dean <hamann.dean@gmail.com>
2018-09-03 23:16:46 -0700

    Adds booltoWord

--- August2.js
+++ August2.js
@@ -292,3 +292,9 @@ function calculateTip(amount, rating) {
 function removeChar(str) {
   return str.slice(1, -1);
 }
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  return bool ? 'Yes':'No';
+}
