6daf94ddadc162b13fd283479347e862cb014819
Dean <hamann.dean@gmail.com>
2018-09-11 21:44:59 -0700

    Adds findLongest

--- August2.js
+++ August2.js
@@ -342,3 +342,16 @@ function countArara(n) {
     default: return 'adak ' + countArara(n-2);
   }
 }
+
+// Find the number with the most digits. If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+function findLongest(array){
+  let num = '0';
+  for(let i=0; i<array.length; i++) {
+    let string = array[i].toString();
+    if(string.length > num.length) {
+      num = string;
+    }
+  }
+  return parseInt(num);
+}
