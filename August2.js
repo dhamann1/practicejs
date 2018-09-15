9f4fedb576536a811208a627e0acbb3fdd4eb849
Dean <hamann.dean@gmail.com>
2018-09-14 19:06:42 -0700

    Adds powerOf4

--- August2.js
+++ August2.js
@@ -369,3 +369,12 @@ const minValue = (values) => {
   }
   return Number(lowestNums.join(""));
 }
+
+// Write a method that returns true if a given parameter is a power of 4, and false if it's not. If parameter is not an Integer (eg String, Array) method should return false as well.
+
+function powerOf4(n) {
+  if (n < 4 || n % 4 !== 0) {
+    return false;
+  }
+  return true;
+}
