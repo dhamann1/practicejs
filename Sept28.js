4400f739fd7aa5d03644c7233c9231dfebd11a88
Dean <hamann.dean@gmail.com>
2018-09-29 10:17:55 -0700

    Adds twiceAsOld

--- Sept28.js
+++ Sept28.js
@@ -1,3 +1,12 @@
 // Define a method/function that removes from a given array of integers all the values contained in a second array.
 
 Array.prototype.remove_ = (a, b) => a.filter(num => !b.includes(num));
+
+// Your function takes two arguments:
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(2 * sonYearsOld - dadYearsOld);
+}
