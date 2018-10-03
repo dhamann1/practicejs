5a0740d23fe80e1252152deb5a10ef0703ec244a
Dean <hamann.dean@gmail.com>
2018-10-02 20:23:14 -0700

    Adds repeater

--- Sept28.js
+++ Sept28.js
@@ -18,3 +18,9 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 function hero(bullets, dragons){
   return bullets >= dragons * 2
 }
+
+// Write a class function named repeat() that takes two arguments (a string and a long integer), and returns a new string where the input string is repeated that many times.
+
+function repeater(string, n){
+  return string.repeat(n)
+}
