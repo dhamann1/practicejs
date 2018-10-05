9f043a5376878cd6435c07612fab8425e78dfd59
Dean <hamann.dean@gmail.com>
2018-10-04 20:22:07 -0700

    Adds inviteMoreWomen

--- Sept28.js
+++ Sept28.js
@@ -40,3 +40,22 @@ function switcheroo(string){
 function take(arr, n) {
   return arr.slice(0, n);
 }
+
+// Arthur needs you to return true if he needs to invite more women or false if he is all set.
+
+// Input/Output
+// [input] integer array L ($a in PHP)
+
+// An array (guaranteed non-associative in PHP) representing the genders of the attendees, where -1 represents women and 1 represents men.
+
+// 2 <= L.length <= 50
+
+// [output] a boolean value
+
+// true if Arthur need to invite more women, false otherwise.
+
+function inviteMoreWomen(L) {
+  let sum = 0
+  L.forEach(e=>sum+=e)
+  return sum > 0
+}
