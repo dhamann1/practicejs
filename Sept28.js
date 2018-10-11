da87ed6b25c4d6ce6aed0e9116b01f6232293295
Dean <hamann.dean@gmail.com>
2018-10-10 19:41:07 -0700

    Adds calculate

--- Sept28.js
+++ Sept28.js
@@ -106,3 +106,11 @@ function swapArray(arr) {
   }
   return arr
 }
+
+// In this kata, you will do addition and subtraction on a given string. The return value must be a 'string'.
+// Example: calculate('1plus2plus3minus4') should return '2'
+
+function calculate(str) {
+let newStr = str.replace(/plus/g, '+').replace(/minus/g, '-');
+return eval(newStr).toString();
+}
