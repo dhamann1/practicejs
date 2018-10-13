2458366843d565167fb56ec7e51c8bac80acd049
Dean <hamann.dean@gmail.com>
2018-10-13 12:33:51 -0700

    Adds maxMultiple

--- Sept28.js
+++ Sept28.js
@@ -120,4 +120,17 @@ function copyList (l) {
   return l.map(e => e)
 };
 
-const copyList = l => l.slice(); 
+const copyList = l => l.slice();
+
+// Given a Divisor and a Bound , Find the largest integer N , Such That ,
+// Conditions :
+// N is divisible by divisor
+// N is less than or equal to bound
+// N is greater than 0.
+// Notes
+// The parameters (divisor, bound) passed to the function are only positve values .
+// It's guaranteed that a divisor is Found.
+
+function maxMultiple(divisor, bound) {
+  return Math.floor(bound / divisor) * divisor;
+}
