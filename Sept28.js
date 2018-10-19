ad5a04673cb4d4a3f8bc288bf35d9ee5667382ac
Dean <hamann.dean@gmail.com>
2018-10-18 20:28:35 -0700

    Adds productArray

--- Sept28.js
+++ Sept28.js
@@ -181,3 +181,17 @@ const arrayPreviousLess = arr => {
     return i;
   });
 }
+
+
+
+
+// Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+// Notes
+// Array/list size is at least 2 .
+// Array/list's numbers Will be only Postives
+// Repeatition of numbers in the array/list could occur.
+
+const productArray = numbers => {
+  let p = numbers.reduce ((a,b) => a*b);
+  return numbers.map(x => p/x);
+}
