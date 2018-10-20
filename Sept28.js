5a48896d63ad2e01e8ae45fbe3baba31ed3698ee
Dean <hamann.dean@gmail.com>
2018-10-19 20:04:00 -0700

    Adds points

--- Sept28.js
+++ Sept28.js
@@ -209,3 +209,23 @@ const oddCount = n => {
   }
   return count;
 }
+
+
+// Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the array.
+// For example: ["3:1", "2:2", "0:1", ...]
+// Write a function that takes such list and counts the points of our team in the championship. Rules for counting points for each match:
+// if x>y - 3 points
+// if x<y - 0 point
+// if x=y - 1 point
+
+function points(games) {
+  let total = 0;
+  games.map(game => {
+    if (game[0] === game[2]) {
+      total += 1;
+    } else if (game[0] > game[2]) {
+      total += 3;
+    }
+  });
+  return total;
+}
