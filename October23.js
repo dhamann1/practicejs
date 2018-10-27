ce746f322a4c30fbaaf5a16a3214974ae156ad46
Dean <hamann.dean@gmail.com>
2018-10-26 20:35:37 -0700

    Adds switcher

--- October23.js
+++ October23.js
@@ -8,3 +8,11 @@ const roundToFive = numbers => {
 // Given an array of numbers, return the difference between the largest and smallest values.
 
 const betweenExtremes = numbers => Math.max(...numbers) - Math.min(...numbers);
+
+// Given an array of numbers, you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+const switcher = x => {
+ let alphabet = "zyxwvutsrqponmlkjihgfedcba!? ";
+ return x.map(e => alphabet[parseInt(e) - 1]).join('');
+
+}
