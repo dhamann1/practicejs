14fb7613ab638f20aafd9f3edb89722bfc4977e2
Dean <hamann.dean@gmail.com>
2018-10-31 22:59:31 -0700

    Adds evaporator

--- October23.js
+++ October23.js
@@ -21,3 +21,20 @@ const switcher = x => {
 const sumStr = (a,b) => {
   return String(Number(a) + Number(b));
 }
+
+// This program tests the life of an evaporator containing a gas.
+//
+// We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+//
+// The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+
+function evaporator(content, evap_per_day, threshold){
+var days = 0;
+var gas = 100;
+  while(gas >= threshold){
+    gas -= gas * evap_per_day / 100;
+    days++;
+}
+  return days;
+}
