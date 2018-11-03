72979ad5b85ac78cfb3aa2f25c0fdcab864e4028
Dean <hamann.dean@gmail.com>
2018-11-03 14:36:48 -0700

    Adds doubleTrouble

--- October23.js
+++ October23.js
@@ -1,3 +1,4 @@
+
 Given an array of numbers, return an array, with each member of input array rounded to a nearest number, divisible by 5.
 
 const roundToFive = numbers => {
@@ -54,4 +55,24 @@ function tram(stops, offboarding, onboarding){
     }
   }
   return capacity;
+}
+
+// Given an array of integers (x), and a target (t), you must find out if any two consecutive numbers in the array sum to t. If so, remove the second number.
+// Example:
+// x = [1, 2, 3, 4, 5]
+// t = 3
+// 1+2 = t, so remove 2. No other pairs = t, so rest of array remains:
+// [1, 3, 4, 5]
+// Work through the array from left to right.
+// Return the resulting array.
+
+function doubleTrouble(x, t){
+  for (let i = 0; i < x.length; i++) {
+    let k = i + 1;
+    if (x[i] + x[k] === t) {
+      x.splice(k, 1);
+      i--;
+    }
+  }
+  return x;
 }
