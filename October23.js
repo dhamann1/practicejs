af6405d93ce3b20bfffbf94186e61d7ace8efc4e
Dean <hamann.dean@gmail.com>
2018-11-04 23:32:39 -0800

    Adds amIWilson

--- October23.js
+++ October23.js
@@ -1,5 +1,5 @@
-
-Given an array of numbers, return an array, with each member of input array rounded to a nearest number, divisible by 5.
+Given an array of numbers,
+  return an array, with each member of input array rounded to a nearest number, divisible by 5.
 
 const roundToFive = numbers => {
   return numbers.map(elem => Math.round(elem / 5) * 5);
@@ -13,13 +13,13 @@ const betweenExtremes = numbers => Math.max(...numbers) - Math.min(...numbers);
 // Given an array of numbers, you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
 
 const switcher = x => {
- let alphabet = "zyxwvutsrqponmlkjihgfedcba!? ";
- return x.map(e => alphabet[parseInt(e) - 1]).join('');
+  let alphabet = "zyxwvutsrqponmlkjihgfedcba!? ";
+  return x.map(e => alphabet[parseInt(e) - 1]).join('');
 }
 
 // Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
 
-const sumStr = (a,b) => {
+const sumStr = (a, b) => {
   return String(Number(a) + Number(b));
 }
 
@@ -31,13 +31,13 @@ const sumStr = (a,b) => {
 // The program reports the nth day (as an integer) on which the evaporator will be out of use.
 
 
-function evaporator(content, evap_per_day, threshold){
-var days = 0;
-var gas = 100;
-  while(gas >= threshold){
+function evaporator(content, evap_per_day, threshold) {
+  var days = 0;
+  var gas = 100;
+  while (gas >= threshold) {
     gas -= gas * evap_per_day / 100;
     days++;
-}
+  }
   return days;
 }
 
@@ -45,7 +45,7 @@ var gas = 100;
 // Calculate the tram's minimum capacity such that the number of people inside the tram never exceeds this capacity at any time. Note that at each stop all exiting passengers exit before any entering passenger enters the tram
 
 
-function tram(stops, offboarding, onboarding){
+function tram(stops, offboarding, onboarding) {
   let capacity = 0;
   let passengers = 0;
   for (let i = 0; i < stops; ++i) {
@@ -66,7 +66,7 @@ function tram(stops, offboarding, onboarding){
 // Work through the array from left to right.
 // Return the resulting array.
 
-function doubleTrouble(x, t){
+function doubleTrouble(x, t) {
   for (let i = 0; i < x.length; i++) {
     let k = i + 1;
     if (x[i] + x[k] === t) {
@@ -75,4 +75,20 @@ function doubleTrouble(x, t){
     }
   }
   return x;
+}
+
+
+// Wilson primes satisfy the following condition. Let P represent a prime number.
+// Then ((P-1)! + 1) / (P * P) should give a whole number.
+// Your task is to create a function that returns true if the given number is a Wilson prime.
+
+
+function amIWilson(p) {
+  var n = (factorial(p-1) + 1) / (p*p);
+  return n === parseInt(n);
+}
+
+function factorial(n) {
+  if (n === 0) return 1;
+  return n * factorial(n-1);
 }
