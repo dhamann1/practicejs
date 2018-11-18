f614d68345ebebddb0763d87dd470c67e6683450
Dean Hamann <hamann.dean@gmail.com>
2018-11-17 21:34:21 -0800

    Adds capitalsFirst

--- October23.js
+++ October23.js
@@ -136,3 +136,14 @@ function formatWords(words) {
     return a + ",";
   }).join(" ").slice(0, -1);
 }
+
+
+// Create a function that takes an input String and returns a String, where all the uppercase words of the input String are in front and all the lowercase words at the end. The order of the uppercase and lowercase words should be the order in which they occur.
+
+
+const capitalsFirst = str => {
+  let words = str.split(' ');
+  let upper = words.filter(function(x) { return x.charAt(0).match(/[A-Z]/) });
+  let lower = words.filter(function(x) { return x.charAt(0).match(/[a-z]/) });
+  return upper.concat(lower).join(' ');
+}
