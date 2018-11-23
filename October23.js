3fc346b7683d55137c5f10536fa80d6dcef38c65
Dean Hamann <hamann.dean@gmail.com>
2018-11-23 11:20:03 -0800

    Adds litres

--- October23.js
+++ October23.js
@@ -147,3 +147,7 @@ const capitalsFirst = str => {
   let lower = words.filter(function(x) { return x.charAt(0).match(/[a-z]/) });
   return upper.concat(lower).join(' ');
 }
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling. You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+const litres = time => Math.floor(time * .5);
