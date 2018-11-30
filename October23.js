c24cc1a1b814ee072778de2abad9710e383d7cb5
Dean Hamann <hamann.dean@gmail.com>
2018-11-29 23:22:11 -0800

    Adds popShift

--- October23.js
+++ October23.js
@@ -168,3 +168,21 @@ const numberToString = num => String(num);
 // Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
 
 const any = (arr, fun) => arr.some(fun);
+
+
+// You will be given a string.
+// You need to return an array of three strings by gradually pulling apart the string.
+// You should repeat the following steps until the string length is 1:
+// a) remove the final character from the original string, add to solution string 1. b) remove the first character from the original string, add to solution string 2.
+// The final solution string value is made up of the remaining character from the original string, once originalstring.length == 1.
+
+function popShift(s){
+  let result =['','',''];
+  let arr = s.split('');
+  while (arr.length > 1) {
+    result[1]+= arr.shift();
+    result[0]+= arr.pop();
+  }
+  result[2] = arr[0] || '';
+  return result;
+}
