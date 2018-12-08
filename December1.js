ad7fa208a26fbeb0da192cb052396584624cf6b9
Dean <hamann.dean@gmail.com>
2018-12-07 19:40:58 -0800

    Adds isUpperCase

--- December1.js
+++ December1.js
@@ -1,15 +1,15 @@
 // Given an array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order
 
-const flattenAndSort = array => [].concat(...array).sort((a,b)=>a-b)
+const flattenAndSort = array => [].concat(...array).sort((a, b) => a - b)
 
 
 // There is a sentence which has a mistake in it's ordering.
 //
 // The part with a capital letter should be the first word.
 
-function reOrdering(text){
-let newArray = []
-for(word of text.split(' ')){
+function reOrdering(text) {
+  let newArray = []
+  for (word of text.split(' ')) {
     word[0] === word[0].toUpperCase() ? newArray.unshift(word) : newArray.push(word)
   }
   return newArray.join(' ').toString()
@@ -18,12 +18,18 @@ for(word of text.split(' ')){
 
 // remove every second element out of that array. Always keep the first element and start removing with the next element.
 
-const removeEveryOther = items =>{
-  return items.filter((item,index) => index % 2 === 0);
+const removeEveryOther = items => {
+  return items.filter((item, index) => index % 2 === 0);
 }
 
 // Find the anonymous function in the given array and use the function to filter the array
 
-function FindFunction(func, arr){
+function FindFunction(func, arr) {
   return arr.filter(func.filter(f => typeof f == 'function')[0]);
+}
+
+// Create a method is_uppercase() to see whether the string is ALL CAPS.
+
+String.prototype.isUpperCase = function() {
+  return this == this.toUpperCase() ? true : false;
 }
