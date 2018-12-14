ae81d2d16b4eba5020c9edf11eb8d08f8ae796ac
Dean <hamann.dean@gmail.com>
2018-12-13 20:05:55 -0800

    Adds summation

--- December1.js
+++ December1.js
@@ -66,3 +66,8 @@ function personalise(campaign, person){
   campaign = campaign.replace(/<FAVOURITE\sPRODUCTS>/g,person.favourite_products);
   return campaign;
 }
+
+
+// Finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+const summation = n => n * (n + 1) / 2;
