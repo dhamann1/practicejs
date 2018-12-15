e10216bda6cf3da0679e272afd3dbfd76833822b
Dean <hamann.dean@gmail.com>
2018-12-14 19:47:58 -0800

    Adds inAscOrder

--- December1.js
+++ December1.js
@@ -71,3 +71,7 @@ function personalise(campaign, person){
 // Finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
 
 const summation = n => n * (n + 1) / 2;
+
+// Determine whether the numbers are in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
+
+const inAscOrder = arr => arr.join('') === arr.sort((a, b) => a - b).join('');
