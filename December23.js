2a34a4c9e916fcd998e37e652b137ead58621959
Dean Hamann <hamann.dean@gmail.com>
2018-12-25 15:32:21 -0800

    Adds evenNumbers

--- December23.js
+++ December23.js
@@ -1,2 +1,6 @@
 // Remove n exclamation marks in the sentence from left to right. n is positive integer.
 const removeNExclamation = (s, n) => n ? remove(s.replace('!', ''), n - 1) : s;
+
+// Given an array of digitals numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+const evenNumbers = (array, number) => array.filter(x => x % 2 === 0).slice(-number)
