ef79a65e2bd2407b26b18060b0990e86a860aeac
Dean Hamann <hamann.dean@gmail.com>
2018-12-25 16:20:53 -0800

    Adds oddBall

--- December23.js
+++ December23.js
@@ -4,3 +4,9 @@ const removeNExclamation = (s, n) => n ? remove(s.replace('!', ''), n - 1) : s;
 // Given an array of digitals numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
 
 const evenNumbers = (array, number) => array.filter(x => x % 2 === 0).slice(-number)
+
+// Given an array with several "even" words, one "odd" word, and some numbers mixed in.
+
+// Determine if any of the numbers in the array is the index of the "odd" word. If so, return true, otherwise false.
+
+const oddBall = array => array.includes(array.indexOf("odd")); 
