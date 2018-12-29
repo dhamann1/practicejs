987566cc7f8bb8deaf3ffaa51aa21ae81e886c3c
Dean Hamann <hamann.dean@gmail.com>
2018-12-28 16:48:08 -0800

    Adds bucketOf

--- December23.js
+++ December23.js
@@ -14,3 +14,22 @@ const oddBall = array => array.includes(array.indexOf("odd"));
 // A magic index in an array A[1...n-1] is defined to be an index such that A[i] = i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
 
 const findMagic = arr => arr.findIndex(i => arr[i] === i);
+
+
+// If you say any of the following words a large bucket of "water" will be dumped on you: "water", "wet", "wash" This is true for any form of those words, like "washing", "watered", etc.
+
+// If you say any of the following phrases you will be doused in "slime": "I don't know", "slime"
+
+// If you say both in one sentence, a combination of water and slime, "sludge", will be dumped on you.
+
+// Write a function, bucketOf(str), that takes a string and determines what will be dumped on your head. If you haven't said anything you shouldn't have, the bucket should be filled with "air". The words should be tested regardless of case.
+
+function bucketOf(str) {
+  str = str.toLowerCase();
+  let water = /water|wet|wash/.test(str);
+  let slime = /i don't know|i dont know|slime/.test(str);
+  if (water && slime) return "sludge";
+  if (water) return "water";
+  if (slime) return "slime";
+  else return "air";
+}
