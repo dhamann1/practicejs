06e905a3eec114f0181030bee2060970674769b0
Dean <hamann.dean@gmail.com>
2018-12-29 13:03:52 -0800

    Adds capitalizeWord

--- December23.js
+++ December23.js
@@ -14,3 +14,8 @@ const oddBall = array => array.includes(array.indexOf("odd"));
 // A magic index in an array A[1...n-1] is defined to be an index such that A[i] = i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
 
 const findMagic = arr => arr.findIndex(i => arr[i] === i);
+
+// Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
+// Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
+
+const capitalizeWord = word => word[0].toUpperCase() + word.slice(1);
