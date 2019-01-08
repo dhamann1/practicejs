a6346ec580a12c32d73539abe52afc2bb865601e
Dean <hamann.dean@gmail.com>
2019-01-07 23:04:26 -0800

    Adds capitalize

--- Jan6.js
+++ Jan6.js
@@ -6,3 +6,14 @@ function bingo(a) {
 }
 
 const bingo = arr => [2,7,9,14,15].every(num => arr.includes(num)) ? "WIN" : "LOSE";
+
+
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+// The input will be a lowercase string with no spaces.
+
+function capitalize(s){
+  const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+  const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+  return [even, odd];
+};
