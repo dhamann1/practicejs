dd6ea616b939119623d5f004eb9308761a460424
Dean <hamann.dean@gmail.com>
2019-01-11 19:36:21 -0800

    Adds hero

--- Jan6.js
+++ Jan6.js
@@ -30,3 +30,18 @@ const setAlarm = (employed, vacation) => employed && !vacation;
 
 // Given a string, return true if the first instance of "x" in the string is immediately followed by the string "xx".
 const tripleX = str => str.substr(str.indexOf('x')+1, 2) === 'xx';
+
+// In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. The hero should have the following attributes:
+// attribute	value
+// name	user argument or 'Hero'
+// position	'00'
+// health	100
+// damage	5
+// experience	0
+function Hero(name){
+  this.name = name ? name : "Hero";
+  this.position = "00";
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
+}
