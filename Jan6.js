050b3861c65b9913a38c2aa661e12d6ae6e773f6
Dean <hamann.dean@gmail.com>
2019-01-22 23:42:32 -0800

    Adds objectType

--- Jan6.js
+++ Jan6.js
@@ -2,17 +2,17 @@
 // Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array). Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
 
 function bingo(a) {
-  return [2,9,14,7,15].every(x => a.includes(x)) ? "WIN" : "LOSE"
+  return [2, 9, 14, 7, 15].every(x => a.includes(x)) ? "WIN" : "LOSE"
 }
 
-const bingo = arr => [2,7,9,14,15].every(num => arr.includes(num)) ? "WIN" : "LOSE";
+const bingo = arr => [2, 7, 9, 14, 15].every(num => arr.includes(num)) ? "WIN" : "LOSE";
 
 
 // Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
 // For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
 // The input will be a lowercase string with no spaces.
 
-function capitalize(s){
+function capitalize(s) {
   const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
   const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
   return [even, odd];
@@ -29,7 +29,7 @@ const onlyDuplicates = str => str.split('').filter(letter => str.indexOf(letter)
 const setAlarm = (employed, vacation) => employed && !vacation;
 
 // Given a string, return true if the first instance of "x" in the string is immediately followed by the string "xx".
-const tripleX = str => str.substr(str.indexOf('x')+1, 2) === 'xx';
+const tripleX = str => str.substr(str.indexOf('x') + 1, 2) === 'xx';
 
 // In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. The hero should have the following attributes:
 // attribute	value
@@ -38,10 +38,19 @@ const tripleX = str => str.substr(str.indexOf('x')+1, 2) === 'xx';
 // health	100
 // damage	5
 // experience	0
-function Hero(name){
+function Hero(name) {
   this.name = name ? name : "Hero";
   this.position = "00";
   this.health = 100;
   this.damage = 5;
   this.experience = 0;
+}
+
+
+// Bob has written a function to detect the type of an object. It receives any primitive values or objects, and returns its object type as a string. But it seems to be working incorrectly. Can you figure out why, and fix the code for him?
+
+function objectType() {
+  return Object.prototype.toString.call(
+    arguments.length ? arguments[0] : null
+  );
 }
