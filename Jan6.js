bef1ab35bc1fe59d597d8c487fbad65e0251917d
Dean Hamann <hamann.dean@gmail.com>
2019-01-30 22:00:11 -0800

    Adds enough

--- Jan6.js
+++ Jan6.js
@@ -72,3 +72,18 @@ function elevator(left, right, call) {
 
 //Answer 2
 const elevator = (left, right, call) => Math.abs(left-call) < Math.abs(right-call) ? "left" : "right";
+
+
+// You have to write a function that accepts three parameters:
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus.
+// wait is the number of people waiting to get on to the bus.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+function enough(cap, on, wait) {
+  let passengerCount = on + wait; 
+  if (passengerCount <= cap) return 0;
+  else return passengerCount - cap; 
+}
+
+const enough = (cap, on, wait) => (on + wait > cap) ? on+wait-cap : 0; 
