74049594f12267f7c8671587c01e26f72535bdf5
Dean <hamann.dean@gmail.com>
2019-02-06 23:36:54 -0800

    Adds twoSort

--- Jan6.js
+++ Jan6.js
@@ -105,3 +105,8 @@ const removeUrlAnchor = url => url.split('#')[0];
 // Return your answer as a number.
 
 const sumMix = x => x.map(num => +num).reduce((a,b) => a + b);
+
+// You will be given an vector of string(s). You must sort it alphabetically (case-sensitive!!) and then return the first value.
+// The returned value must be a string, and have "***" between each of its letters. You should not remove or add elements from/to the array.
+
+const twoSort = list => list.sort()[0].split('').join('***'); 
