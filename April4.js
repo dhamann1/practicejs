283491b5f9591c974a4b9ad7279eedef41fe94c3
Dean <hamann.dean@gmail.com>
2019-02-15 19:57:43 -0800

    Adds refactored factorial

--- April4.js
+++ April4.js
@@ -5,6 +5,8 @@ function factorialRecursion(n) {
   return n * factorial(n - 1);
 }
 
+const factorial = n => n === 0 ? 1 : n * factorial (n-1); 
+
 // Place all people in alphabetical order where Mothers are followed by their children.I.E "aAbaBb" => "AaaBbb".
 
 function findChildren(dancingBrigade) {
