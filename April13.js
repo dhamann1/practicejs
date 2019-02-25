ff204c7fd9fd1d92100351cc09a4d7f34a1482a4
Dean <hamann.dean@gmail.com>
2019-02-24 21:13:15 -0800

    Adds refactored pigIt

--- April13.js
+++ April13.js
@@ -20,3 +20,5 @@ function pigIt(str) {
   }).join(' ');
   return splitStr;
 }
+
+const pigIt = s => s.split(' ').map(e => e.substr(1) + e[0] + 'ay').join(' ');
--- April7.js
+++ April7.js
@@ -35,7 +35,6 @@ function uniqueInOrder(iterable) {
 }
 
 
-
 // Given a list of integers and a single sum value, return the first two values (parse from the left please) in order of appearance that add up to form the sum.
 
 function sumPairs(ints, s) {
