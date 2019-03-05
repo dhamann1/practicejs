a791d8d370b24d7899e53103dbbf5cd1dd458ba0
Dean <hamann.dean@gmail.com>
2019-03-04 22:13:32 -0800

    Adds refactored sortGiftCode

--- June7.js
+++ June7.js
@@ -82,6 +82,8 @@ function sortGiftCode(code){
   return code.split('').sort().join('');
 }
 
+const sortGiftCode = code => code.split('').sort().join(''); 
+
 // Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
 
 function sumDigits(number) {
