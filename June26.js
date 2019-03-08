ca1c4989bac7608a8fd347e81b630f66e31d991c
Dean <hamann.dean@gmail.com>
2019-03-07 19:58:19 -0800

    Adds refactored twoOldestAges

--- June26.js
+++ June26.js
@@ -106,9 +106,7 @@ function isSquare(n) {
 
 // The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
 
-function twoOldestAges(ages){
-  return ages.sort(function(a,b){return a-b;}).slice(-2);
-}
+const twoOldestAges = ages => ages.sort((a,b) => a - b).slice(-2);
 
 // When this method is called, it reverses the order of the items in the original array. Then then it returns that same, original array. No new arrays should need to be created to pass this kata.
 
