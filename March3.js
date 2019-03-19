c69791ef2361d52bfb8e4d5fb061ab870a60b1f5
Dean <hamann.dean@gmail.com>
2019-03-18 22:57:31 -0700

    Adds strCount

--- March3.js
+++ March3.js
@@ -1,1 +1,4 @@
-const divisibleBy = (numbers, divisor) => numbers.filter(num => num % divisor === 0); 
+const divisibleBy = (numbers, divisor) => numbers.filter(num => num % divisor === 0);
+
+// Create a function called that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+const strCount = (str,letter) => str.split(letter).length-1; 
