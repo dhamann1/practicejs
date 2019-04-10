80fbd40399c023719e3ff5650a5435f99f0425f9
Dean <hamann.dean@gmail.com>
2019-04-09 20:41:27 -0700

    Adds check

--- March3.js
+++ March3.js
@@ -48,3 +48,7 @@ const calcType = (a, b, res) =>
   }
   return str;
 }
+
+// You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+const check = (a,x) => a.includes(x); 
