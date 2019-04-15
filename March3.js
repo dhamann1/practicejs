cf8cc53f1c72b0f157842fd59f6011786b9f815d
Dean <hamann.dean@gmail.com>
2019-04-14 22:25:00 -0700

    Adds warnTheSheep

--- March3.js
+++ March3.js
@@ -52,3 +52,14 @@ const calcType = (a, b, res) =>
 // You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
 
 const check = (a,x) => a.includes(x); 
+
+// Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//    7      6      5      4      3            2      1
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+// Note: there will always be exactly one wolf in the array.
+
+const warnTheSheep = queue => {
+  let position = queue.reverse().indexOf('wolf');
+  return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+}
