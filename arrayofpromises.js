0f8d9bdfff124c6f91e0933946894837edad4375
Dean <hamann.dean@gmail.com>
2019-04-16 02:02:26 -0700

    Adds arrayofpromises

--- arrayofpromises.js
+++ arrayofpromises.js
@@ -1,21 +1,48 @@
-var promise1 = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve('foo');
-    }, 300);
-  });
-  
-  promise1.then(function(value) {
-    console.log(value);
-    // expected output: "foo"
-  });
-  
-  console.log(promise1);
-  // expected output: [object Promise]
+function getNewHope() {
+    fetch("https://swapi.co/api/films/1/").then(
+      data => data.status !== 200 ? console.log("Status Code: " + data.status) : data.json().then(data => console.log(data))
+    );
+  }
 
-  fetch('http://example.com/movies.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-  });
+  function getLukeSkywalker() {
+    fetch("https://swapi.co/api/films/1/").then(
+      data => data.status !== 200 ? console.log("Status Code: " + data.status) : data.json().then(data => console.log(data))
+    );
+  }
+
+  function getWookie() {
+    fetch("https://swapi.co/api/species/3/").then(
+      data => data.status !== 200 ? console.log("Status Code: " + data.status) : data.json().then(data => console.log(data))
+    );
+  }
+
+  function getTatooine() {
+    fetch("https://swapi.co/api/planets/1/").then(
+      data => data.status !== 200 ? console.log("Status Code: " + data.status) : data.json().then(data => console.log(data))
+    );
+  }
+
+function set10Seconds () {
+  return new Promise ((resolve,reject) => setTimeout (()=> resolve('This took 10 seconds'), 10000)).then(data => console.log(data));
+}
+
+
+//   Reduce collapses the array of promises into a single promise chain. Allows to run the promises sequentail as opposed to in parallel with Promise.All()
+
+let dataObject = {
+    funcList: [],
+    addStep: function addStep(func) {
+      this.funcList.push(func);
+    },
+    runList: function runList () {
+        return this.funcList.reduce((promiseChain, currentTask) => {
+            return promiseChain.then(chainResults =>
+                currentTask.then(currentResult =>
+                    [ ...chainResults, currentResult ]
+                )
+            );
+        }, Promise.resolve([])).then(arrayOfResults => {
+            console.log(arrayOfResults);
+        });
+    }
+};
