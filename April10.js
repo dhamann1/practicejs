89b0b63ce797fea5c3a259f1b301f5a840252013
Dean <hamann.dean@gmail.com>
2019-04-25 22:52:14 -0700

    Adds grow

--- April10.js
+++ April10.js
@@ -6,4 +6,6 @@ const integrate = (coefficient, exponent) =>  coefficient / (exponent + 1) + 'x^
 
 const getAverage = marks => Math.floor(marks.reduce((a, b) => a + b) / marks.length);
 
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
+const grow = (nums) => nums.reduce((product, num) => product * num, 1);
