0e86613709a3e4011e6784295a31a37bd5218721
Dean <hamann.dean@gmail.com>
2019-05-01 19:10:29 -0700

    Adds shiftLeft

--- April10.js
+++ April10.js
@@ -9,3 +9,21 @@ const getAverage = marks => Math.floor(marks.reduce((a, b) => a + b) / marks.len
 // Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
 const grow = (nums) => nums.reduce((product, num) => product * num, 1);
+
+// You are given two strings. In a single move, you can choose any of them, and delete the first (i.e. leftmost) character.
+// For Example:
+// By applying a move to the string "where", the result is the string"here"`.
+// By applying a move to the string "a", the result is an empty string""`.
+// Implement a function that calculates the minimum number of moves that should be performed to make the given strings equal.
+// Notes
+// Both strings consist of lowercase latin letters.
+// If the string is already empty, you cannot perform any more delete operations.
+
+function shiftLeft(s, t){
+  let count = 0;
+  while (s != t){
+    s.length > t.length ? s = s.slice(1) : t = t.slice(1);
+    count++
+  }
+  return count
+}
