8d2b6a8f05baa2a45b9d0e8ac3e01a07dcb21d39
Dean <hamann.dean@gmail.com>
2019-05-02 23:50:54 -0700

    Edits promises

--- promises/promises.js
+++ promises/promises.js
@@ -1,3 +1,9 @@
+function doMath () {
+  return 5 + 5;
+};
+
+
+
 function getLukeSkywalker() {
     return fetch("https://swapi.co/api/films/1/").then(data => data.json()).then(data => data.title);
 }
@@ -20,20 +26,37 @@ function getFilmTitle(x) {
     })
 }
 
+function changeFilmTitle (x) {
+  x = x + " is a great movie.";
+  return x;
+}
+
 let dataObject = {
     pList: [],
     addStep: function addStep(func) {
       this.pList.push(func);
     },
     runList: function runList () {
-        return this.pList.reduce((previousPromise, nextPromise) => previousPromise.then(nextPromise), Promise.resolve())
+      return this.pList.reduce((accumulator, currentVal) => {
+        if (accumulator instanceof Promise) {
+          return accumulator.then(currentVal);
+        }
+        else {
+          return accumulator(currentVal);
+        };
+      })
     }
 };
 
-
+dataObject.addStep(doMath);
 dataObject.addStep(getLukeSkywalker);
+dataObject.addStep(changeFilmTitle);
 dataObject.addStep(passFilmTitle);
 dataObject.addStep(getFilmTitle);
 
+
+
+
+console.log(getLukeSkywalker() instanceof Promise);
 
 dataObject.runList().then(data => console.log("results: ", data));
