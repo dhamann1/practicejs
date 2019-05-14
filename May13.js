462ae44af7ea203dddc4c22cb3182495aa8203a4
Dean <hamann.dean@gmail.com>
2019-05-13 22:33:59 -0700

    Adds orderedCount

--- May13.js
+++ May13.js
@@ -5,8 +5,7 @@ const sumR = x => x.reduce ((a,b)=> a+b,0);
 // Count the number of occurrences of each character and return it as a list of tuples in order of appearance.
 
 var orderedCount = function (text) {
-    var letters = text.split("")
-    var uniqs = letters.filter((letter, index) => letters.indexOf(letter) == index)
-    
-    return uniqs.map((letter) => [letter, text.split(letter).length - 1])
-  }
+    var letters = text.split("");
+    var uniqs = letters.filter((letter, index) => letters.indexOf(letter) == index);
+    return uniqs.map((letter) => [letter, text.split(letter).length - 1]);
+  };
