b5f8540109e599ae3348671611c6fd70c6e2c016
Dean <hamann.dean@gmail.com>
2019-05-17 18:20:03 -0700

    Edits promisesDL

--- promises/promisesDL.js
+++ promises/promisesDL.js
@@ -1,7 +1,7 @@
 function getObj () {
-  return {  
+  return {
     series_brand: 'Toyota',
-    series_year: 2020, 
+    series_year: 2020,
     user_zipcode: 90803
   };
 };
@@ -27,15 +27,15 @@ function addUserFacebook(x) {
 };
 
 function changeSeriesYear (x) {
-  x.series_year = 2018; 
+  x.series_year = 2018;
   // console.log('This is Step 3: ', x);
-  return x; 
+  return x;
 };
 
 function addDealerCity(x) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
-            x.dealer_city = 'Long Beach'; 
+            x.dealer_city = 'Long Beach';
             // console.log('This is Step 4: ', x);
             resolve(x);
         }, 2000);
@@ -43,9 +43,9 @@ function addDealerCity(x) {
 };
 
 function addUserContactPreference (x) {
-  x.user_contact_preference = 'email'; 
+  x.user_contact_preference = 'email';
   // console.log('This is Step 5: ', x);
-  return x; 
+  return x;
 };
 
 
@@ -57,28 +57,23 @@ let dataObject = {
     runList: function runList () {
       return this.pList.reduce((accumulator, currentVal) => {
         if (accumulator instanceof Promise) {
-          console.log('we are in an async function');
+          console.log('we will run async function');
           return accumulator.then(currentVal);
         }
-        else {
-          console.log('we are in a sync function');
+        else if (accumulator instanceof Function) {
+          console.log('we will run sync function');
           return currentVal(accumulator());
         };
+        return currentVal(accumulator);
       })
     }
 };
 
 
-dataObject.addStep(getObj); 
-
-dataObject.addStep(addUserFacebook);
+dataObject.addStep(getObj);
 dataObject.addStep(changeSeriesYear);
+dataObject.addStep(addUserFacebook);
 dataObject.addStep(addDealerCity);
 dataObject.addStep(addUserContactPreference);
-
-
- 
-
-
 
 dataObject.runList().then(data => console.log("These are the results: ", data));
