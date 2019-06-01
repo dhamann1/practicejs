471bb2832132a1706dd5a86b95d2298395aba686
Dean <hamann.dean@gmail.com>
2019-05-31 19:02:51 -0700

    Adds cannonsReady

--- May13.js
+++ May13.js
@@ -52,3 +52,14 @@ function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
   var ed = new Date(expirationDate);
   return (enteredCode === correctCode) && (ed >= cd);
 }
+
+// Your task is to check if the gunners are loaded and ready, if they are: Fire!
+// If they aren't ready: Shiver me timbers!
+// Your gunners for each test case are 4 or less.
+// When you check if they are ready their answers are in a dictionary and will either be: aye or nay
+// Firing with less than all gunners ready is non-optimum (this is not fire at will, this is fire by the captain's orders or walk the plank, dirty sea-dog!)
+// If all answers are 'aye' then Fire! if one or more are 'nay' then Shiver me timbers!
+
+const cannonsReady = gunners => {
+  return Object.values(gunners).every(response => response === 'aye') ? 'Fire!' : 'Shiver me timbers!'
+}
