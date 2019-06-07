2cf157b4164d817acddbd113ae662c84edbba72f
Dean <hamann.dean@gmail.com>
2019-06-06 18:47:20 -0700

    Adds updateLight

--- May13.js
+++ May13.js
@@ -70,8 +70,22 @@ const formatMoney = amount => '$' + amount.toFixed(2);
 
 const otherAngle = (a, b) => 180 - a - b;
 
-Return the Nth Even Number
+// Return the Nth Even Number
 
-// const nthEven=n=>n*2-2;
+const nthEven=n=>n*2-2;
 
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
 
+function updateLight(current) {
+  switch (current) {
+    case 'green':
+      return 'yellow';
+    case 'yellow':
+      return 'red';
+    case 'red':
+      return 'green';
+    default:
+      throw 'Error: wrong input';
+  }
+}
