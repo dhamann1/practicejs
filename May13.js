d02c20c4c9c10123061452a45c653fe1421dc350
Dean <hamann.dean@gmail.com>
2019-06-10 22:07:08 -0700

    Adds betterThanAverage

--- May13.js
+++ May13.js
@@ -89,3 +89,10 @@ function updateLight(current) {
       throw 'Error: wrong input';
   }
 }
+
+// You got an array with your colleges' points. Now calculate the average to your points!
+// Return True if you're better, else False!
+
+function betterThanAverage(classPoints, yourPoints) {
+  return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
+}
