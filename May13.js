9390bdd3c653e5adc4ae26615d78b28034345294
Dean <hamann.dean@gmail.com>
2019-06-25 22:09:09 -0700

    Adds sabb

--- May13.js
+++ May13.js
@@ -109,3 +109,14 @@ function mergeArrays(arr1, arr2) {
 function paddedNumbers(value){
   return "Value is " + ("00000" + value).slice(-5);
 }
+
+
+
+// You need to approach your boss. Her decision will be based on three parameters:
+// val=your value to the organisation
+// happ=her happiness level at the time of asking and finally
+// The numbers of letters from 'sabbatical' that are present in string 'x'.
+// Note that if x contains three instances of the letter 'l', that still scores three points, even though there is only one in the word sabbatical.
+// If the sum of the three parameters (as described above) is > 22, return 'Sabbatical! Boom!', else return 'Back to your desk, boy.'.
+
+const sabb = (x, val, happ) => x.split('').reduce((s,a)=> s + 'sabbatical'.includes(a) , 0) + val + happ > 22 ? 'Sabbatical! Boom!' :  'Back to your desk, boy.';
