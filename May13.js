b7d49bb779164a0d7b11cee9866199217945ca21
Dean <hamann.dean@gmail.com>
2019-07-22 21:55:44 -0700

    Adds maxProduct

--- May13.js
+++ May13.js
@@ -108,3 +108,10 @@ function minimumSteps(numbers, value) {
     }
   }
 }
+
+
+// Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+function maxProduct(numbers, size){
+  return numbers.sort((a, b) => b - a).slice(0, size).reduce((a, b) => a * b);
+}
