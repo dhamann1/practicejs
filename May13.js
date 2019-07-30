c271299ece00a6c532b81091914c341aaebca5c4
Dean <hamann.dean@gmail.com>
2019-07-29 23:02:27 -0700

    Adds isNice

--- May13.js
+++ May13.js
@@ -227,3 +227,14 @@ const arrayLeaders = numbers => {
   
   return leaders;
 }
+
+
+// A Nice array is defined to be an array where for every value n in the array, there is also an element n-1 or n+1 in the array.
+// Write a function named isNice/IsNice that returns true if its array argument is a Nice array, else false. You should also return false if input array has no elements.
+
+function isNice(arr) {
+  if (arr.length === 0) {
+      return false;
+  }
+  return arr.every(item => arr.includes(item + 1) || arr.includes(item - 1))
+}
