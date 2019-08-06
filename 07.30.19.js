d6a9c0dd4ccf00fa317679a2dcaaffede81e965e
Dean <hamann.dean@gmail.com>
2019-08-05 21:41:10 -0700

    Adds sumOfABeach

--- 07.30.19.js
+++ 07.30.19.js
@@ -1,3 +1,9 @@
 // In this Kata, you will remove the left-most duplicates from a list of integers and return the result.
 
 const removeDuplicates = row => row.filter((x, i) => row.lastIndexOf(x) === i)
+
+// Beaches are filled with sand, water, fish, and sun. Given a string, calculate how many times the words "Sand", "Water", "Fish", and "Sun" appear without overlapping (regardless of the case).
+
+function sumOfABeach (beach) {
+    return beach.split(/sand|water|fish|sun/gi).length - 1
+  }
