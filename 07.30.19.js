8bf535b052ffc312b73058551015acf1afa75d6c
Dean <hamann.dean@gmail.com>
2019-09-05 16:43:56 -0700

    Adds scoreboard

--- 07.30.19.js
+++ 07.30.19.js
@@ -85,3 +85,27 @@ const nthSmallest = (a, n) => a.sort((x, y) => x - y)[n - 1];
 function oddOne(arr) {
   return arr.findIndex((num) => (num % 2 != 0))
 }
+
+// You are working at a lower league football stadium and you've been tasked with automating the scoreboard.
+// The referee will shout out the score, you have already set up the voice recognition module which turns the ref's voice into a string, but the spoken score needs to be converted into a pair for the scoreboard!
+// e.g. "The score is four nil" should return [4,0]
+// Either teams score has a range of 0-9, and the ref won't say the same string every time e.g.
+
+
+function scoreboard (string) {
+  var numbers = {
+    'nil': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+  };
+  var scoreOne = string.split(' ').reverse()[1];
+  var scoreTwo = string.split(' ').reverse()[0];
+  return [numbers[scoreOne], numbers[scoreTwo]];
+}
