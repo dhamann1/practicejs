7522705539eb483aba9d07e5f45c5a3c5da2b450
Dean <hamann.dean@gmail.com>
2019-09-18 22:16:05 -0700

    Adds getAges

--- 07.30.19.js
+++ 07.30.19.js
@@ -164,3 +164,16 @@ function generateRange(min, max, step){
     });
     return nums.filter(y => numCount[y] % 2 === 0);
   }
+
+  // Create a function that takes in the sum and age difference of two people, calculates their individual ages, and returns a pair of values (oldest age first) if those exist or null/None if:
+  // sum < 0
+  // difference < 0
+  // Either of the calculated ages come out to be negative
+
+function getAges (sum,difference){
+  if (sum < 0 || difference < 0 ) {return null;}
+  var x = (sum + difference)/2; 
+  var y = sum - x; 
+  if (x < 0 || y < 0){return null;}
+  return [x,y];
+}
