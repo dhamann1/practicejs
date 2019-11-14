ef07be4680372bc467b893f406e2f3afccb59451
Dean <hamann.dean@gmail.com>
2019-11-13 22:41:59 -0800

    Adds numberTest

--- 11.04.19.js
+++ 11.04.19.js
@@ -9,3 +9,17 @@ function getGrade (s1, s2, s3) {
     else if (avg < 90) return "B";
     else return "A";
 }
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+
+function numberTest (array) {
+  if (array.length <= 0) {
+      return [];
+  } else {
+      return array.map((x,index) => {
+          return index + 1 + ": " + x;
+      })
+  }
+}
