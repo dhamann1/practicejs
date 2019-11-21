22796c2729c2530f32f202d211657dbe4b52eb90
Dean <hamann.dean@gmail.com>
2019-11-20 20:06:41 -0800

    Adds humanYearsCatYearsDogYears

--- 11.04.19.js
+++ 11.04.19.js
@@ -34,3 +34,35 @@ function cockroachSpeed(s) {
   
   return Math.floor((s*centsInKilo)/secsInHour);
 }
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+function humanYearsCatYearsDogYears(humanYears) {
+  var catYears = 0;
+  var dogYears = 0;
+  
+  if (humanYears === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (humanYears === 2) {
+    catYears = 24;
+    dogYears = 24;
+  } else if (humanYears > 2) {
+    catYears = 24 + (humanYears - 2) * 4;
+    dogYears = 24 + (humanYears - 2) * 5;
+  }
+  return [humanYears, catYears, dogYears];
+}
