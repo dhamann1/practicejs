61d1892b680d296775e9644ba8390837a9dfb1dc
Dean <hamann.dean@gmail.com>
2019-12-30 19:07:26 -0800

    Adds between

--- 11.04.19.js
+++ 11.04.19.js
@@ -92,3 +92,13 @@ function lottery(str){
   str = str.replace(/[a-z]/gi, '');
   return str.length > 0 ? str.split('').filter((value, index) => str.indexOf(value) == index).join('') : "One more run!";
 }
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+function between (a, b) {
+  var list = []; 
+  for(var i = a; i <= b; i++) {
+    list.push(i);
+  }
+  return list; 
+} 
