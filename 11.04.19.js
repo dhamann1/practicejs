99aa5db62226f9fd54cdbfcbb904d65ad1103e58
Dean <hamann.dean@gmail.com>
2020-01-10 20:14:14 -0800

    Adds houseNumbersSum

--- 11.04.19.js
+++ 11.04.19.js
@@ -140,3 +140,19 @@ function willYou (young, beautiful, loved) {
     return true;
   }
 }
+
+
+// For the given sequence of houses determine the sum that the boy will get. It is guaranteed that there will always be at least one 0 house on the path.
+// Example
+// For inputArray = [5, 1, 2, 3, 0, 1, 5, 0, 2], the output should be 11.
+// The answer was obtained as 5 + 1 + 2 + 3 = 11.
+
+function houseNumbersSum(inputArray) {
+  for (var sum = 0, i = 0;; i++)
+    if (inputArray[i] === 0) {
+    return sum;
+    }
+    else {
+    sum += inputArray[i];
+  }
+}
