4acf2d963b1eca4b272ecbcf9f709aa886e4ac4a
Dean <hamann.dean@gmail.com>
2020-01-14 18:28:10 -0800

    Adds hoopCount

--- 11.04.19.js
+++ 11.04.19.js
@@ -169,3 +169,7 @@ function findLongest(str) {
     }
     return longest;
 }
+
+// -If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// -If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+const hoopCount = n => (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
