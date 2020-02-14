6dac56bc97a8f9a283d82d99d6bd233b3de5f4a4
Dean <hamann.dean@gmail.com>
2020-02-13 20:01:46 -0800

    Adds paperwork

--- 02.10.20.js
+++ 02.10.20.js
@@ -3,3 +3,14 @@
 function countLettersAndDigits(s) {
     return s.replace(/[^a-z\d]/gi,"").length;
   }
+
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+// Your task is to calculate how many blank pages do you need.
+
+function paperwork(n, m) {
+    if (m < 0 || n < 0) {
+      return 0;
+    }
+    return m * n;
+  }
