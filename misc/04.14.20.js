7b33452a03fecccd1648b5914cb6b729e246d3e5
Dean <hamann.dean@gmail.com>
2020-04-15 21:11:15 -0700

    Adds halvingSum

--- misc/04.14.20.js
+++ misc/04.14.20.js
@@ -26,3 +26,24 @@ function missingValues(arr) {
     }
     return num1 * num1 * num2;  
   }
+
+  /*
+  Given a positive integer n, calculate the following sum:
+
+  n + n/2 + n/4 + n/8 + ...
+  All elements of the sum are the results of integer division.
+  
+  Example
+  25  =>  25 + 12 + 6 + 3 + 1 = 47
+  */
+
+
+  function halvingSum(n) {
+    var sum = 0;
+    while(n > 0) {
+      sum += n;
+      n = Math.floor(n / 2);
+      console.log(n); 
+    }
+    return sum;
+  }
