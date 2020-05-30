e1b11320cc5d4c2782c3a7a8325a11e10ea14371
Dean <hamann.dean@gmail.com>
2020-05-29 21:54:49 -0700

    Adds sortReindeer

--- misc/05.26.20.js
+++ misc/05.26.20.js
@@ -5,3 +5,13 @@ function capital (capitals) {
       return 'The capital of ' + (x.state || x.country) + " is " + x.capital;  
     });
   }
+
+  // Write a function that accepts a sequence of Reindeer names, and returns a sequence with the Reindeer names sorted by their last names.
+
+  function sortReindeer(reindeerNames) {
+    return reindeerNames.sort((name1, name2) => lastName(name1) > lastName(name2));
+  }
+  
+  function lastName(name) {
+    return name.split(' ')[1];
+  }
