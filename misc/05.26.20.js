6320a6d36c91e54124164d3daff325c1198a49ae
Dean <hamann.dean@gmail.com>
2020-08-18 23:06:31 -0700

    Adds evenChars

--- misc/05.26.20.js
+++ misc/05.26.20.js
@@ -52,5 +52,15 @@ function capital (capitals) {
   function stringChallenge(string, a, b) {
     return string.slice(0, a) + string.slice(a, b + 1).split('').reverse().join('') + string.slice(b + 1)
   }
+
+  // Write a function that returns a sequence (index begins with 1) of all the even characters from a string. If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string".
   
-  
+  function evenChars (string) {
+    if (string.length < 2 || string.length > 100){
+      return "invalid string";
+    } else {
+      return string.split("").filter((item,index) => {
+        return index % 2 !== 0; 
+      });
+    }
+  }
