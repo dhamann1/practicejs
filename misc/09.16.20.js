8f70aaa687783f57c13bed5312ceb3a98d1d7b70
Dean <hamann.dean@gmail.com>
2020-09-29 23:44:14 -0700

    Adds removeElement

--- misc/09.16.20.js
+++ misc/09.16.20.js
@@ -3,3 +3,16 @@
 var strStr = function(haystack, needle) {
     return (haystack.indexOf(needle) ? haystack.indexOf(needle) : 0); 
 };
+
+// Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+var removeElement = function(nums, val) {
+    var a = 0;
+    for(var i = 0; i < nums.length; i++){
+        if (nums[i] !== val){
+            nums[a] = nums[i];
+            a++;    
+        }
+    }
+    return a;   
+};
